f20daf24f16114608c116d825dcec0c4d4cd42f1
zeevvax <zeev_vax@hotmail.com>
2014-10-22 16:48:47 -0700

    Initial Commit

---
+++ TripLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TripLogger
+//
+//  Created by Zeev Vax on 10/22/14.
+//  Copyright (c) 2014 Zeev Vax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
